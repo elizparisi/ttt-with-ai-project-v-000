57b613a13a7d4bc62be064b105ab9a430828f80d
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-07-18 01:13:35 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,8 +2,16 @@ module Players
   class Computer < Player
     
     def move(board)
-      move = nil 
-      (1..9).to_a.sample.to_s
+      
+      if board.turn_count == 0
+        ["1", "3", "7", "9"].sample
+      else
+        proposed_move = (1..9).to_a.sample.to_s
+        until !board.taken?(proposed_move)
+          proposed_move = (1..9).to_a.sample.to_
+        end
+        proposed_move
+        end
     end
   end
 end
